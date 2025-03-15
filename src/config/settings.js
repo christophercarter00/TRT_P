bf49064b7f972e162117504f68ad5d43fb517899
Christopher Carter <ccar6234@gmail.com>
2025-03-15 11:13:07 -0400

    feat: update dependencies and modify theme settings for The Roam Theory

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	title: `The Roam Theory | Because there’s no growth if you don’t roam.`,
+	description: `Roam Theory is a journey into how travel and movement shape personal growth, exploring the transformative moments found between destinations.`,
+	url: `https://theroamtheory.com`,
+	name: `The Roam Theory`,
+	enableThemeSwitcher: false,
 };
